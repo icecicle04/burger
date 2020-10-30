4cdc45367d297493a1c83e29135fe7271a26d462
AK <icecicle04@gmail.com>
2020-10-30 17:25:45 -0400

    added schema and seeds file

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+-- Create a new database called 'burgers_db'
+
+CREATE DATABASE burgers_db
+GO
+-- drop any prior database
+DROP DATABASE IF EXISTS burgers_db;
+
+-- creates new database for burgers
+CREATE DATABASE burgers_db;
+-- using new database
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+    burger_name VARCHAR,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY
+    (id)
+    );
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('big cheese', true);
