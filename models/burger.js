160ecd150def50c09c630ae831acaeab8a0e4fb4
AK <icecicle04@gmail.com>
2020-10-30 18:59:53 -0400

    finalized models file

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+// importing ORM
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function (cb) {
+    orm.all("burgers", function (res) {
+      cb(res);
+    });
+  },
+  // variable cols and vals
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  update: function (objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
+  delete: function (condition, cb) {
+    orm.delete("burgers", condition, function (res) {
+      cb(res);
+    });
+  },
+};
