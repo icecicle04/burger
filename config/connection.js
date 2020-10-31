54b0c1a43e17b6bf8d2dcc7a6cff13afded52d74
AK <icecicle04@gmail.com>
2020-10-30 23:28:29 -0400

    fixed connection thread id

--- config/connection.js
+++ config/connection.js
@@ -16,7 +16,7 @@ connection.connect(function (err) {
     console.log("error connecting: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadID);
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use
