55d57cf8e77594b874be3521610bdb2e9d082e5e
AK <icecicle04@gmail.com>
2020-10-30 21:27:20 -0400

    added module export

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -47,3 +47,17 @@ router.put("api/burgers/:id", function (req, res) {
     }
   );
 });
+
+router.delete("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function (result) {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
