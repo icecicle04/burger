1504d6bad01a65dff6793cf2024b5c71b4962a95
AK <icecicle04@gmail.com>
2020-10-30 21:24:33 -0400

    added router puts

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -26,3 +26,24 @@ router.post("/api/burgers", function (req, res) {
     }
   );
 });
+
+router.put("api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    function (result) {
+      if (result.changedRows == 0) {
+        // if no rows were changed, then the ID must not exist so err
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
