bcb4d465992b2e13fbcc0890c2267cc2fc91c039
AK <icecicle04@gmail.com>
2020-11-02 13:02:50 -0500

    swapped server file with working budget tracker file to see if it works on heroku

--- server.js
+++ server.js
@@ -1,40 +1,26 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const compression = require("compression");
 
-require("dotenv").config();
-
-let PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Parse application body as JSON
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/burger-app-ak",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-  }
-);
-// Set Handlebars.
-const exphbs = require("express-handlebars");
+app.use(express.static("public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-const routes = require("./controllers/burgersController.js");
-
-app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, () => {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
 });
-//add mongouri
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
