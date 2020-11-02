cca9f564d67022246e992c64c266e5ba68c5d50f
AK <icecicle04@gmail.com>
2020-11-02 13:15:49 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# burger
-Lets the user pick a burger that they want to eat and then renders the burger for them to eat.
+# Burger App
+
+![<ALT>](https://img.shields.io/badge/Burger-App-blue)
+
+## Description
+
+An application that lets display conceptual burgers which you can in turn devour.
+
+## Table of Contents
+
+- Installation
+- Usage
+- License
+- Contributors
+- Tests
+- Questions
+
+## Installation
+
+https://burger-app-ak.herokuapp.com/ - current build is not deployed, best way to use it is on local host through the repository here: https://github.com/icecicle04/burger
+
+## Usage
+
+Input the new burger item you would like to devour. Click add burger. Once the burger is added to the list, you can then devour the burger and it will move to the devoured column.
+
+## Licenses
+
+MIT License
+
+## Contributors
+
+Ali Khan
+
+## Tests
+
+Tested on Local Host
+
+## Questions
+
+email Ali.Khan4990@gmail.com
+
+## Images
+
+![<ALT>](public/assets/images/homepage.png)
--- server.js
+++ server.js
@@ -1,26 +1,40 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+
+let PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(compression());
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/burger-app-ak",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  }
+);
+// Set Handlebars.
+const exphbs = require("express-handlebars");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () => {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
-
-// routes
-app.use(require("./routes/api.js"));
-
-app.listen(process.env.PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+//add mongouri
